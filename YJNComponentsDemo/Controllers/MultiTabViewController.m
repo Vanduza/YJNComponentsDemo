844a7a65e901cda5e73ab53d266f1a7b455218dd
YangJing <yangjing2580@163.com>
2018-05-19 06:45:11 +0800

    测试标题不等宽的MultiTabView表现

--- YJNComponentsDemo/Controllers/MultiTabViewController.m
+++ YJNComponentsDemo/Controllers/MultiTabViewController.m
@@ -8,6 +8,7 @@
 
 #import "MultiTabViewController.h"
 #import "YJNMultiTabView.h"
+#import "Masonry.h"
 
 @interface MultiTabViewController ()<YJNMultiTabDelegate>
 @property (nonatomic, strong) YJNMultiTabView *tabView;
@@ -21,19 +22,25 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
-    _tabView = [[YJNMultiTabView alloc] initWithFrame:CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, 44)];
+    _tabView = [[YJNMultiTabView alloc] init];//WithFrame:CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, 44)];
     _tabView.delegate = self;
     _tabView.needUnderline = NO;
     _tabView.indicatorHeight = 5;
     _tabView.indicatorWidth = 60;
     _tabView.backgroundColor = [UIColor cyanColor];
     NSMutableArray *titles = [[NSMutableArray alloc] initWithCapacity:5];
-    for (int i=0; i < 13; i++) {
-        NSString *title = [NSString stringWithFormat:@"Tab %d",i];
-        [titles addObject:title];
-    }
+//    for (int i=0; i < 4; i++) {
+//        NSString *title = [NSString stringWithFormat:@"Tab %d",i];
+//        [titles addObject:title];
+//    }
+    titles = @[@"  待处理的订单  ",@"  供料中的工程  ",@"  已取消  ",@"  已完成  "].mutableCopy;
     _tabView.tabTitles = titles;
     [self.view addSubview:_tabView];
+    [_tabView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.view);
+        make.top.equalTo(self.view.mas_topMargin);
+        make.height.mas_equalTo(40);
+    }];
     
     UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 120, 80, 50)];
     btn.backgroundColor = [UIColor blueColor];
